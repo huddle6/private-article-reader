5962b40196676b5094a1f9dfe9fcebab4ff84f2f
huddle6 <196012465+huddle6@users.noreply.github.com>
2025-01-28 10:18:30 +0700

    Update Footer.tsx

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-primary text-white p-4 text-center">
+    <footer className="bg-gray-100 text-black p-4 text-center">
       <p>© {new Date().getFullYear()} Private Article Reader. All rights reserved.</p>
     </footer>
   );
