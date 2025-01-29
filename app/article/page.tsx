f6fed1e1450c20508ea816c7b7092d5ed50a8162
huddle6 <196012465+huddle6@users.noreply.github.com>
2025-01-29 11:21:18 +0700

    Update page.tsx

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,57 +1,46 @@
-"use client";
-
-import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
-import { Roboto } from 'next/font/google';
-import { useSearchParams, redirect } from 'next/navigation';
-import { ArticleData, extract } from '@extractus/article-extractor';
-import { FiFileText, FiMic, FiUser, FiWatch } from 'react-icons/fi';
+import React from "react";
+import Link from "next/link";
+import { Roboto } from "next/font/google";
+import { redirect } from "next/navigation";
+import { ArticleData, extract } from "@extractus/article-extractor";
+import { FiFileText, FiMic, FiUser, FiWatch } from "react-icons/fi";
 import validator from 'validator';
 
-const roboto = Roboto({ subsets: ['latin'], weight: '300' });
+const roboto = Roboto({ subsets: ["latin"], weight: "300" });
 
-const getArticle = async (article_url: string | null, userAgent: string) => {
+const getArticle = async (article_url: string | null) => {
   try {
     if (article_url === null || !validator.isURL(article_url)) {
-      throw new Error('The provided URL is invalid. Please check the URL and try again.');
+      throw new Error("The provided URL is invalid. Please check the URL and try again.");
     }
 
-    const parserOptions = {
-      wordsPerMinute: 300,
-      descriptionTruncateLen: 210,
-      descriptionLengthThreshold: 180,
-      contentLengthThreshold: 200,
-    };
-
-    // Scraping and preparing article with user-agent header.
-    const article = await extract(article_url, parserOptions, {
-      headers: {
-        'user-agent': userAgent,
-      },
-    });
+    // Scraping and preparing article.
+    const article = await extract(article_url);
 
     // Returning parsed article data in props to UI.
     return article;
   } catch (error) {
     if (error instanceof Error) {
       console.error(`Error fetching article: ${error.message}`);
-      if (error.message.includes('INVALID URL')) {
-        throw new Error('The provided URL is invalid. Please check the URL and try again.');
+      if (error.message.includes("INVALID URL")) {
+        throw new Error("The provided URL is invalid. Please check the URL and try again.");
       } else {
-        throw new Error('An error occurred while fetching the article. Please try again later.');
+        throw new Error("An error occurred while fetching the article. Please try again later.");
       }
     } else {
-      console.error('An unknown error occurred.');
-      throw new Error('An unknown error occurred. Please try again later.');
+      console.error("An unknown error occurred.");
+      throw new Error("An unknown error occurred. Please try again later.");
     }
   }
 };
 
-const ArticleImage: React.FC<{ article: ArticleData }> = ({ article }) => {
+const ArticleImage: React.FC<{article: ArticleData}> = ({article}) => {
   if (article.image) {
     return (
       <div>
-        {article.content?.includes(article.image) ? null : (
+        {article.content?.includes(article.image) ? (
+          <></>
+        ) : (
           <img
             src={article.image}
             alt={article.title}
@@ -62,76 +51,79 @@ const ArticleImage: React.FC<{ article: ArticleData }> = ({ article }) => {
     );
   }
 
-  return null;
+  return <></>;
 };
 
-const ArticlePage: React.FC = () => {
-  const [article, setArticle] = useState<ArticleData | null>(null);
-  const [error, setError] = useState<string | null>(null);
-  const searchParams = useSearchParams();
-  const url = searchParams.get('url');
-  const userAgent = searchParams.get('userAgent');
+const ArticlePage = async ({
+  searchParams,
+}: {
+  searchParams: { url: string };
+}) => {
+  try {
+    const url = searchParams.url;
+    // Loading article
+    const article = await getArticle(url);
+    if (!article) {
+      throw new Error("The article could not be loaded. Please check the URL and try again.");
+    }
 
-  useEffect(() => {
-    const fetchArticle = async () => {
-      try {
-        if (url && userAgent) {
-          const fetchedArticle = await getArticle(url, userAgent);
-          setArticle(fetchedArticle);
-        } else {
-          redirect('/error'); // Redirect to an error page if URL or userAgent is invalid
-        }
-      } catch (err) {
-        setError(err instanceof Error ? err.message : 'An unknown error occurred.');
-      }
-    };
-
-    fetchArticle();
-  }, [url, userAgent]);
-
-  if (error) {
-    return <div>{error}</div>;
-  }
-
-  if (!article) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div>
+    return (
       <div>
-        <Link href={article.source || '/'}>
-          <a className="flex items-center justify-center gap-2 py-1 my-2 font-bold text-center underline bg-yellow-500 item">
+        <div>
+          {/* Read at source. */}
+          <Link
+            href={article.source || "/"}
+            className="flex items-center justify-center gap-2 py-1 my-2 font-bold text-center underline bg-yellow-500 item"
+          >
             <FiFileText /> <span>Read at source.</span>
-          </a>
-        </Link>
+          </Link>
 
-        <div className="px-2">
-          <h2 className={`my-8 text-2xl font-bold text-center ${roboto.className}`}>
-            {article.title}
-          </h2>
+          {/* Page Content. */}
+          <div className="px-2">
+            {/* Title */}
+            <h2 className="my-8 text-2xl font-bold text-center">
+              {article.title}
+            </h2>
 
-          <ArticleImage article={article} />
+            {/* Article main image. */}
+            <ArticleImage article={article} />
 
-          <div className="flex flex-row justify-center gap-6 mt-4">
-            <div className="flex flex-row items-center gap-2">
-              <FiUser /> {article.author || 'No author found.'}
+            {/* Basic Info. */}
+            <div className="flex flex-row justify-center gap-6 mt-4">
+              <div className="flex flex-row items-center gap-2">
+                <FiUser /> {article.author || "No author found."}
+              </div>
+
+              <div className="flex flex-row items-center gap-2">
+                <FiWatch />{" "}
+                {new Date(article.published || "").toLocaleDateString() ||
+                  "Publishing time not found."}
+              </div>
+
+              <div className="flex flex-row items-center gap-2">
+                <FiMic />{" "}
+                {`${Math.round(Number(article.ttr) / 60)} Minutes` || "0 Minutes"}
+              </div>
             </div>
 
-            <div className="flex flex-row items-center gap-2">
-              <FiWatch /> {new Date(article.published || '').toLocaleDateString() || 'Publishing time not found.'}
-            </div>
-
-            <div className="flex flex-row items-center gap-2">
-              <FiMic /> {`${Math.round(Number(article.ttr) / 60)} Minutes` || '0 Minutes'}
-            </div>
+            {/* Parsed article body. */}
+            <article
+              className={"mx-auto my-0 prose max-w-6xl " + roboto.className}
+              dangerouslySetInnerHTML={{ __html: String(article.content) }}
+            ></article>
           </div>
-
-          <article className={`mx-auto my-0 prose max-w-6xl ${roboto.className}`} dangerouslySetInnerHTML={{ __html: String(article.content) }} />
         </div>
       </div>
-    </div>
-  );
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(`Error rendering article page: ${error.message}`);
+      return <div>An error occurred: {error.message}</div>;
+    } else {
+      console.error("An unknown error occurred.");
+      return <div>An unknown error occurred. Please try again later.</div>;
+    }
+  }
 };
 
 export default ArticlePage;
