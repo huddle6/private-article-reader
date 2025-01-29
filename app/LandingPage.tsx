c55e1502455781ef3e01464cb6daf4a19acd4c93
huddle6 <196012465+huddle6@users.noreply.github.com>
2025-01-29 11:21:05 +0700

    Update LandingPage.tsx

--- app/LandingPage.tsx
+++ app/LandingPage.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
-import { useRouter } from 'next/router';
+import React, { useState } from "react";
 
 const how_it_works = [
   {
@@ -10,7 +9,7 @@ const how_it_works = [
   },
   {
     id: 2,
-    text: "Our bot goes and fetches the page and takes out article content and strips everything else including trackers."
+    text: "Our bot goes and fetches the page and takes out article content and strips everything else including trackers.",
   },
   {
     id: 3,
@@ -20,97 +19,38 @@ const how_it_works = [
 
 const LandingPage = () => {
   const [isCollapsed, setIsCollapsed] = useState(true);
-  const [history, setHistory] = useState<{ link: string, date: string }[]>([]);
-  const [urlInput, setUrlInput] = useState('');
-  const router = useRouter();
-
-  useEffect(() => {
-    const savedHistory = localStorage.getItem('linkHistory');
-    if (savedHistory) {
-      setHistory(JSON.parse(savedHistory));
-    }
-  }, []);
 
   const toggleCollapse = () => {
     setIsCollapsed(!isCollapsed);
   };
 
-  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const newEntry = { link: urlInput, date: new Date().toISOString() };
-    let history = JSON.parse(localStorage.getItem('linkHistory') || '[]');
-
-    // Remove duplicates
-    history = history.filter((entry: { link: string }) => entry.link !== urlInput);
-    history.unshift(newEntry);
-    if (history.length > 100) history = history.slice(0, 100);
-    localStorage.setItem('linkHistory', JSON.stringify(history));
-    setHistory(history);
-
-    const userAgent = navigator.userAgent;
-    router.push({
-      pathname: '/article',
-      query: { url: urlInput, userAgent: userAgent }
-    });
-  };
-
   return (
     <div className="px-2">
-      {/* Main form */}
+      {/* Main form. */}
       <div className="my-4 border-b-2">
         <form
-          onSubmit={handleFormSubmit}
-          className="flex items-center gap-4 my-4"
+          action="/article"
+          method="GET"
+          className="flex flex-col items-center gap-4 my-4"
         >
           <input
             type="url"
             name="url"
-            value={urlInput}
-            onChange={(e) => setUrlInput(e.target.value)}
             required
-            className="flex-1 px-3 py-2 border-2 rounded outline-none"
+            className="flex-1 min-w-full px-3 py-2 border-2 rounded outline-none"
             placeholder="Enter article URL here."
           />
           <button
             type="submit"
-            className="px-4 py-2 text-white bg-black rounded hover:bg-gray-800"
+            className="min-w-full px-4 py-2 text-white bg-black rounded hover:bg-gray-800"
           >
             Load Article
           </button>
         </form>
       </div>
 
-      {/* History section */}
-      <div className="my-4 border-b-2">
-        <div className="flex justify-between items-center">
-          <h2 className="text-lg font-bold">History</h2>
-          <button
-            onClick={() => {
-              localStorage.removeItem('linkHistory');
-              setHistory([]);
-            }}
-            className="px-4 py-2 text-white bg-red-600 rounded hover:bg-red-800"
-          >
-            Clear History
-          </button>
-        </div>
-        <ul className="max-h-40 overflow-y-auto">
-          {history.map((entry, index) => (
-            <li key={index} className="flex justify-between">
-              <span
-                onClick={() => setUrlInput(entry.link)}
-                className="cursor-pointer text-blue-600"
-              >
-                {entry.link}
-              </span>
-              <span>{new Date(entry.date).toLocaleString('en-GB')}</span>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      {/* How it works */}
-      <div className="flex flex-col gap-4 px-4 py-4 rounded shadow-lg">
+      {/* How it works. */}
+      <div className="flex flex-col gap-4 px-4 py-4 bg-white rounded shadow-lg">
         <h2 
           className="text-lg font-bold cursor-pointer text-left focus:outline-none"
           onClick={toggleCollapse}
