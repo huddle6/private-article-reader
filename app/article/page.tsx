cd56beef9759fe3cb6525bd44a3590b75b63f2cf
huddle6 <196012465+huddle6@users.noreply.github.com>
2025-01-30 14:57:54 +0700

    Update page.tsx

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -76,9 +76,9 @@ const ArticlePage = async ({
         <div>
           {/* Read at source. */}
           <Link
-            href={(new URL(article.source)).href}
+            href={article.source ? (new URL(article.source)).href : "#"}
             className="flex items-center justify-center gap-2 py-1 my-2 font-bold text-center underline bg-yellow-500 item"
-          >
+            >
             <FiFileText /> <span>Read at source.</span>
           </Link>
 
